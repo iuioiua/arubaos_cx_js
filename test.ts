eb82f82e736cb4d73f3dc95401b8c2e60b4c690f
Asher Gomez <ashersaupingomez@gmail.com>
2022-07-15 09:38:35 +1000

    fixed: testing

--- test.ts
+++ test.ts
@@ -1,9 +1,9 @@
-import { requestOnce } from "./mod.ts";
+import { fetchOnce } from "./mod.ts";
 import { assert } from "./deps.ts";
 
 //** All environmental variables need to be defined */
 Deno.test("requestOnce", async () => {
-  const response = await requestOnce({
+  const response = await fetchOnce({
     host: Deno.env.get("ARUBAOS_CX_HOST")!,
   }, "/system");
   assert(response.ok);
